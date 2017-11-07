88e91e35c1711294ee775272eda8c15d78cefc45
Sky Mason <skymason0@yahoo.com>
2017-11-07 09:46:43 -0600

    sections one to six finished

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require( 'express' );
 const app = express(); 
+const nunjucks = require('nunjucks');
 
 
 app.listen(3000, ()=>{
   console.log('Running on port 3000');
 })
+
 
 app.use('/', (req, res, next)=>{
   console.log(req.method + ' ' + req.url)
@@ -12,5 +14,22 @@ app.use('/', (req, res, next)=>{
 })
 
 app.get('/',(req, res)=>{
-  res.send('<h1>Hello Message</h1>')
+  // res.send('<h1>Hello Message</h1>')
+nunjucks.render('index.html', locals, function(err, output){
+	res.send(output);
+});
+
 })
+
+var locals = {
+	title: 'An Example',
+	people: [
+	{name: 'Gandalf'},
+	{name: 'Frodo'},
+	{name: 'Hermione'}
+	]
+};
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+
