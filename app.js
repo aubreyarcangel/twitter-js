c0d5e14d0e218c1d9c1f81abc1ee9fccefc1b7f5
Sky Mason <skymason0@yahoo.com>
2017-11-07 10:30:50 -0600

    working live test

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require( 'express' );
 const app = express(); 
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const path = require('path');
 
 
 app.listen(3000, ()=>{
   console.log('Running on port 3000');
 })
+
+app.use('/', routes);
+
+// app.get('/public', function(req, res){
+// 	res.sendFile(__dirname + '/public/stylesheets/style.css');
+// })
+
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 
 app.use('/', (req, res, next)=>{
@@ -13,22 +23,23 @@ app.use('/', (req, res, next)=>{
   next();
 })
 
-app.get('/',(req, res)=>{
-  // res.send('<h1>Hello Message</h1>')
-nunjucks.render('index.html', locals, function(err, output){
-	res.send(output);
-});
+// app.get('/',(req, res)=>{
+//   // res.send('<h1>Hello Message</h1>')
+// nunjucks.render('index.html', locals, function(err, output){
+// 	res.send(output);
+// });
 
-})
+// })
 
-var locals = {
-	title: 'An Example',
-	people: [
-	{name: 'Gandalf'},
-	{name: 'Frodo'},
-	{name: 'Hermione'}
-	]
-};
+// var locals = {
+// 	title: 'An Example',
+// 	people: [
+// 	{name: 'Gandalf'},
+// 	{name: 'Frodo'},
+// 	{name: 'Hermione'}
+// 	]
+// };
+
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true});
